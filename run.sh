881ec0961cba88446f542798c0e932e44a752bf1
jieyuanCUHK <40020029+jieyuanCUHK@users.noreply.github.com>
2019-03-29 11:26:34 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -30,6 +30,7 @@ python ./02_source_code_for_training/5_training_DeeperCount.py network_parameter
 #need to specify the model parameter you want to use: unet_refined.hdf5|unet.hdf5
 #please pay attention that the unet_refined.hdf5 parameter was trained using multi-gpus, while unet.hdf5 was not. 
 #please change the critical parameters including learning rate, optimization algorithm, epoch number, batch size, etc. according to your experiment.
+#the parameter after transfer learning is ./01_model_parameters/unet_user_new.hdf5.
 
 #6. do prediction:
 #(1) process images that need to predict:
